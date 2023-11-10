c361615787311e3f13e072b8d1eef261475240b3
= <=>
2023-11-11 03:28:08 +0800

    添加默认共享样式(hover,color)

--- src/ui/theme/themes/font.ts
+++ src/ui/theme/themes/font.ts
@@ -1,24 +1,29 @@
 import { CSSProperties } from 'react'
 
 interface IFont {
-	color?: CSSProperties['color']
-	fontSize?: number
-	fontWeight?: number
+  color?: CSSProperties['color']
+  fontSize?: number
+  fontWeight?: number
 }
 
 function _font(color: CSSProperties['color']): IFont
 function _font(color: CSSProperties['color'] | 'default', fontSize: number): IFont
-function _font(color: CSSProperties['color'] | 'default', fontSize: number | 'default', fontWeight: number): IFont
 function _font(
-	color: CSSProperties['color'] | 'default',
-	fontSize?: number | 'default',
-	fontWeight?: number | 'default'
+  color: CSSProperties['color'] | 'default',
+  fontSize: number | 'default',
+  fontWeight: number
+): IFont
+function _font(
+  color: CSSProperties['color'] | 'default',
+  fontSize?: number | 'default',
+  fontWeight?: number | 'default'
 ): IFont {
-	return {
-		...(color !== 'default' && { color }),
-		...(fontSize && fontSize !== 'default' && { fontSize }),
-		...(fontWeight && fontWeight !== 'default' && { fontWeight }),
-	}
+  return {
+    ...(color !== 'default' && { color }),
+    ...(fontSize && fontSize !== 'default' && { fontSize }),
+    ...(fontWeight && fontWeight !== 'default' && { fontWeight }),
+  }
 }
 
 export const font = _font
+export const labelFont = { fontSize: 11, color: 'gray' }
