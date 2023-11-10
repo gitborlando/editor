379f6f5984d7221c49e71e338b9fde86f851c101
= <=>
2023-11-11 03:24:13 +0800

    添加Button小组件

---
+++ src/ui/widget/button.tsx
@@ -0,0 +1,36 @@
+import { useLocalObservable } from 'mobx-react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
+import { makeStyles } from '~/ui/theme'
+import { Flex } from '~/ui/widget/flex'
+
+interface IButton extends ComponentPropsWithRef<'div'> {}
+
+export const Button = forwardRef<HTMLDivElement, IButton>(
+  ({ className, children, ...rest }, ref) => {
+    const { classes, cx } = useStyles({})
+    const {} = useLocalObservable(() => ({}))
+    return (
+      <Flex
+        sidePadding={10}
+        layout='c'
+        className={cx(classes.Button, className)}
+        {...rest}
+        ref={ref}>
+        {children}
+      </Flex>
+    )
+  }
+)
+
+type IButtonStyle = {} /* & Required<Pick<IButton>> */ /* & Pick<IButton> */
+
+const useStyles = makeStyles<IButtonStyle>()((t) => ({
+  Button: {
+    ...t.rect('fit-content', '100%'),
+    ...t.labelFont,
+    ...t.default$.hover.background,
+    cursor: 'pointer',
+  },
+}))
+
+Button.displayName = 'Button'
