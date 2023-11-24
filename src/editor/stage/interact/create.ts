116259e4035ecda5569579e0dbdfb6e7bfc1c223
= <=>
2023-11-24 19:51:28 +0800

    优化画布拖拽创建的逻辑

--- src/editor/stage/interact/create.ts
+++ src/editor/stage/interact/create.ts
@@ -1,4 +1,4 @@
-import { delay, inject, injectable } from 'tsyringe'
+import { inject, injectable } from 'tsyringe'
 import { DragService, injectDrag } from '~/editor/drag'
 import { SchemaDefaultService, injectSchemaDefault } from '~/editor/schema/default'
 import { SchemaNodeService, injectSchemaNode } from '~/editor/schema/node'
@@ -6,7 +6,6 @@ import { SchemaPageService, injectSchemaPage } from '~/editor/schema/page'
 import { INode } from '~/editor/schema/type'
 import { IXY } from '~/editor/utils'
 import { autobind } from '~/helper/decorator'
-import { StageDrawService, injectStageDraw } from '../shape/draw'
 import { StageService, injectStage, listenInteractTypeChange } from '../stage'
 import { ViewportService, injectViewport } from '../viewport'
 
@@ -18,20 +17,16 @@ type ICreateType = typeof createTypes[number]
 export class StageCreateService {
   types = createTypes
   type: ICreateType = 'frame'
-  private _node?: INode
+  private node!: INode
   constructor(
     @injectDrag private dragService: DragService,
     @injectStage private stageService: StageService,
     @injectSchemaDefault private schemaDefaultService: SchemaDefaultService,
     @injectSchemaNode private schemaNodeService: SchemaNodeService,
     @injectSchemaPage private schemaPageService: SchemaPageService,
-    @injectStageDraw private stageDrawService: StageDrawService,
     @injectViewport private viewportService: ViewportService
   ) {
     listenInteractTypeChange(this, 'create')
-  }
-  private get node(): INode {
-    return this._node!
   }
   startInteract() {
     this.dragService
@@ -43,8 +38,7 @@ export class StageCreateService {
         if (this.type === 'line') this.createRect(realStageStart)
         if (this.type === 'text') this.createRect(realStageStart)
         if (this.type === 'img') this.createRect(realStageStart)
-        this.add()
-        this.draw()
+        this.addToSchemaAndSelect()
       })
       .onMove(({ marquee }) => {
         const { x, y } = this.viewportService.toRealStageXY(marquee)
@@ -63,12 +57,12 @@ export class StageCreateService {
           this.node.width = width
           this.node.height = height
         }
-        this.draw()
+        this.schemaNodeService.collectDirty(this.node.id)
       })
       .onEnd(({ dragService: drag }) => {
         if (!this.node.width) this.node.width = 100
         if (!this.node.height) this.node.height = 100
-        this.draw()
+        this.schemaNodeService.collectDirty(this.node.id)
         this.stageService.setInteractType('select')
         drag.endListen()
       })
@@ -79,7 +73,7 @@ export class StageCreateService {
     this.stageService.setInteractType('create')
   }
   private createRect(realStageStart: IXY) {
-    this._node = this.schemaDefaultService.rect({
+    this.node = this.schemaDefaultService.rect({
       x: realStageStart.x,
       y: realStageStart.y,
       width: 0,
@@ -87,21 +81,19 @@ export class StageCreateService {
     })
   }
   private createEllipse(realStageStart: IXY) {
-    this._node = this.schemaDefaultService.ellipse({
+    this.node = this.schemaDefaultService.ellipse({
       x: realStageStart.x,
       y: realStageStart.y,
       width: 0,
       height: 0,
     })
   }
-  private add() {
+  private addToSchemaAndSelect() {
     this.schemaNodeService.add(this.node)
+    this.schemaNodeService.clearSelection()
+    this.schemaNodeService.select(this.node.id)
     this.schemaNodeService.connect(this.node.id, this.schemaPageService.currentId, true)
-  }
-  private draw() {
-    this.stageDrawService.draw(this.node)
   }
 }
 
 export const injectStageCreate = inject(StageCreateService)
-export const delayInjectStageCreate = inject(delay(() => StageCreateService))
