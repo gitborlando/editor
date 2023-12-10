4ece2f821820c5bb888dc2ba18a1cd00416faf9c
= <=>
2023-12-10 14:19:40 +0800

    新增service

--- src/editor/ioc.ts
+++ src/editor/ioc.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata'
 import { container } from 'tsyringe'
-import { DragService } from './drag'
 import { EditorService } from './editor'
 import { FileService } from './file'
 import { SchemaDefaultService } from './schema/default'
@@ -9,13 +8,20 @@ import { SchemaPageService } from './schema/page'
 import { SchemaService } from './schema/schema'
 import { StageCTXService } from './stage/draw/ctx/ctx'
 import { StageDrawService } from './stage/draw/draw'
+import { StageWidgetHoverService } from './stage/draw/widget/hover'
+import { StageWidgetMarqueeService } from './stage/draw/widget/marquee'
+import { StageWidgetTransformerService } from './stage/draw/widget/transformer'
 import { StageElementService } from './stage/element'
 import { StageCreateService } from './stage/interact/create'
 import { StageInteractService } from './stage/interact/interact'
 import { StageMoveService } from './stage/interact/move'
 import { StageSelectService } from './stage/interact/select'
 import { PixiService } from './stage/pixi'
+import { StageTransformerService } from './stage/transformer'
 import { StageViewportService } from './stage/viewport'
+import { DragService } from './utility/drag'
+import { MenuService } from './utility/menu'
+import { SettingService } from './utility/setting'
 
 container.registerSingleton(SchemaDefaultService)
 container.registerSingleton(SchemaNodeService)
@@ -31,8 +37,18 @@ container.registerSingleton(StageCTXService)
 container.registerSingleton(StageDrawService)
 container.registerSingleton(StageElementService)
 container.registerSingleton(StageInteractService)
+container.registerSingleton(StageTransformerService)
+container.registerSingleton(StageWidgetHoverService)
+container.registerSingleton(StageWidgetMarqueeService)
+container.registerSingleton(StageWidgetTransformerService)
+container.registerSingleton(MenuService)
+container.registerSingleton(SettingService)
 container.registerSingleton(EditorService)
 container.registerSingleton(FileService)
+
+container.resolve(StageWidgetHoverService)
+container.resolve(StageWidgetMarqueeService)
+container.resolve(StageWidgetTransformerService)
 
 export const editorServices = {
   SchemaDefault: container.resolve(SchemaDefaultService),
@@ -49,6 +65,9 @@ export const editorServices = {
   StageDraw: container.resolve(StageDrawService),
   StageShape: container.resolve(StageElementService),
   StageInteract: container.resolve(StageInteractService),
+  StageTransformer: container.resolve(StageTransformerService),
+  Menu: container.resolve(MenuService),
+  Setting: container.resolve(SettingService),
   Editor: container.resolve(EditorService),
   File: container.resolve(FileService),
 }
