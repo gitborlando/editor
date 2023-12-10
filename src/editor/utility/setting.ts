6dcabdf67f330f837a484b66a5d1460c8fd97198
= <=>
2023-12-10 14:17:35 +0800

    新增setting模块

---
+++ src/editor/utility/setting.ts
@@ -0,0 +1,28 @@
+import { makeObservable, observable } from 'mobx'
+import { inject, injectable } from 'tsyringe'
+import { autobind } from '~/editor/helper/decorator'
+
+export type ISetting = {
+  color: string
+}
+
+@autobind
+@injectable()
+export class SettingService {
+  @observable setting = <ISetting>{}
+  constructor() {
+    makeObservable(this)
+    this.initSetting()
+  }
+  get color() {
+    return this.setting.color
+  }
+  setSetting(setting: Partial<ISetting>) {
+    this.setting = { ...this.setting, ...setting }
+  }
+  private initSetting() {
+    this.setting.color = '#9166FF'
+  }
+}
+
+export const injectSetting = inject(SettingService)
