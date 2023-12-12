6a6b51e0f10595adca8082ff3cb68dbcb7319d25
= <=>
2023-12-12 13:47:59 +0800

    删除并更新了一些service

--- src/ioc.ts
+++ src/ioc.ts
@@ -6,47 +6,51 @@ import { SchemaDefaultService } from './editor/schema/default'
 import { SchemaNodeService } from './editor/schema/node'
 import { SchemaPageService } from './editor/schema/page'
 import { SchemaService } from './editor/schema/schema'
-import { StageCTXService } from './editor/stage/draw/ctx/ctx'
 import { StageDrawService } from './editor/stage/draw/draw'
 import { StageWidgetHoverService } from './editor/stage/draw/widget/hover'
 import { StageWidgetMarqueeService } from './editor/stage/draw/widget/marquee'
-import { StageWidgetTransformerService } from './editor/stage/draw/widget/transformer'
+import { StageWidgetTransformService } from './editor/stage/draw/widget/transform'
 import { StageElementService } from './editor/stage/element'
 import { StageCreateService } from './editor/stage/interact/create'
 import { StageInteractService } from './editor/stage/interact/interact'
 import { StageMoveService } from './editor/stage/interact/move'
 import { StageSelectService } from './editor/stage/interact/select'
+import { StageTransformService } from './editor/stage/interact/transform'
 import { PixiService } from './editor/stage/pixi'
 import { StageViewportService } from './editor/stage/viewport'
 import { DragService } from './editor/utility/drag'
-import { SettingService } from './editor/utility/setting'
 import { StatusService } from './editor/utility/status'
 import { MenuService } from './global/menu'
+import { SettingService } from './global/setting'
 
-container
+container // schema
   .registerSingleton(SchemaDefaultService)
   .registerSingleton(SchemaNodeService)
   .registerSingleton(SchemaPageService)
   .registerSingleton(SchemaService)
-  .registerSingleton(DragService)
-  .registerSingleton(PixiService)
-  .registerSingleton(StageViewportService)
+container // stage interact
+  .registerSingleton(StageInteractService)
   .registerSingleton(StageSelectService)
   .registerSingleton(StageMoveService)
   .registerSingleton(StageCreateService)
-  .registerSingleton(StageCTXService)
+  .registerSingleton(StageTransformService)
+container // stage
+  .registerSingleton(DragService)
+  .registerSingleton(PixiService)
+  .registerSingleton(StageViewportService)
   .registerSingleton(StageDrawService)
   .registerSingleton(StageElementService)
-  .registerSingleton(StageInteractService)
+container // stage widget
   .registerSingleton(StageWidgetHoverService)
   .registerSingleton(StageWidgetMarqueeService)
-  .registerSingleton(StageWidgetTransformerService)
-  .registerSingleton(SettingService)
+  .registerSingleton(StageWidgetTransformService)
+container // other
   .registerSingleton(StatusService)
   .registerSingleton(EditorService)
   .registerSingleton(FileService)
 
-container.registerSingleton(MenuService)
+// global
+container.registerSingleton(MenuService).registerSingleton(SettingService)
 
 export const editorServices = {
   SchemaDefault: container.resolve(SchemaDefaultService),
@@ -59,19 +63,19 @@ export const editorServices = {
   StageSelect: container.resolve(StageSelectService),
   StageMove: container.resolve(StageMoveService),
   StageCreate: container.resolve(StageCreateService),
-  StageCTX: container.resolve(StageCTXService),
+  StageTransform: container.resolve(StageTransformService),
   StageDraw: container.resolve(StageDrawService),
   StageShape: container.resolve(StageElementService),
   StageInteract: container.resolve(StageInteractService),
-  Setting: container.resolve(SettingService),
   Status: container.resolve(StatusService),
   Editor: container.resolve(EditorService),
   File: container.resolve(FileService),
 }
 container.resolve(StageWidgetHoverService)
 container.resolve(StageWidgetMarqueeService)
-container.resolve(StageWidgetTransformerService)
+container.resolve(StageWidgetTransformService)
 
 export const globalServices = {
   Menu: container.resolve(MenuService),
+  Setting: container.resolve(SettingService),
 }
