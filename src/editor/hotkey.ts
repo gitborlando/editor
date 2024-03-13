f1bf5d604f485e9281294450b91e6ef67dcf3582
= <=>
2024-03-13 15:00:32 +0800

    新增快捷键

--- src/editor/hotkey.ts
+++ src/editor/hotkey.ts
@@ -2,7 +2,7 @@ import autobind from 'class-autobind-decorator'
 import hotkeys, { HotkeysEvent } from 'hotkeys-js'
 import { Record } from '~/editor/record'
 import { createSignal } from '~/shared/signal'
-import { SchemaNode } from './schema/node'
+import { SchemaFile } from './file'
 import { SchemaUtil } from './schema/util'
 
 type IHotKeys = keyof Omit<HotkeyService, 'init' | 'initHook'>
@@ -22,17 +22,13 @@ export class HotkeyService {
     this.bindHotkeys()
     this['ctrl+z'].hook(Record.undo)
     this['ctrl+shift+z'].hook(Record.redo)
-    this['del'].hook(this.onDel)
-  }
-  private onDel() {
-    if (SchemaNode.selectIds.value.size) {
-      SchemaUtil.traverseDelete(SchemaNode.selectIds.value)
-    }
+    this['ctrl+s'].hook(SchemaFile.saveFile)
+    this['del'].hook(SchemaUtil.deleteSelectNodes)
   }
   private bindHotkeys() {
     ;(Object.keys(this) as IHotKeys[]).forEach((hotkey) => {
       hotkeys(hotkey, (keyboardEvent, hotkeysEvent) => {
-        if (hotkey === 'ctrl+s') return keyboardEvent.preventDefault()
+        if (hotkey === 'ctrl+s') keyboardEvent.preventDefault()
         this[hotkey].dispatch({ keyboardEvent, hotkeysEvent })
       })
     })
