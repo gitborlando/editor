262b0306886d94dd9638a7a66f1f0b6ef33c7502
Gitborlando <2629888564@qq.com>
2025-07-02 00:08:22 +0800

    提交

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -3,12 +3,11 @@ import { MenuComp } from 'src/view/component/menu'
 import { UploaderComp } from 'src/view/component/uploader'
 import { Flex } from 'src/view/ui-utility/widget/flex'
 import { EditorComp } from './editor/editor'
+import 'virtual:uno.css'
 
-type IAppProps = {}
-
-export const App: FC<IAppProps> = memo(() => {
+export const App: FC = memo(() => {
   return (
-    <Flex className='lay-v wh-100vw-100vh bg-blue/93' onContextMenu={(e) => e.preventDefault()}>
+    <Flex className='lay-v wh-100vw-100vh' onContextMenu={(e) => e.preventDefault()}>
       <EditorComp />
       <MenuComp />
       <UploaderComp />
