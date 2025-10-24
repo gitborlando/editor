01d5778256e1a0213b36f3bb94aea7dc8ea3df19
Gitborlando <2629888564@qq.com>
2025-10-25 03:28:48 +0800

    修复 undo 组件 UI 问题

--- src/view/editor/left-panel/panels/undo.tsx
+++ src/view/editor/left-panel/panels/undo.tsx
@@ -1,25 +1,29 @@
 import { objectKey } from '@gitborlando/utils'
 import { FC } from 'react'
+import Scrollbars from 'react-custom-scrollbars-2'
 import { Text } from 'src/view/component/text'
 
 export const UndoComp: FC<{}> = observer(({}) => {
   const { next, stack } = YUndo
+
   return (
-    <G style={{ alignContent: 'flex-start' }} gap={0}>
-      {stack.map((item, i) => (
-        <G
-          key={objectKey(item)}
-          style={{
-            width: '100%',
-            height: 32,
-            alignContent: 'center',
-            paddingLeft: 8,
-            color: next === i + 1 ? 'rgba(0, 100, 255)' : '#000000',
-            backgroundColor: next === i + 1 ? 'rgba(0, 100, 255, 0.1)' : '#FFFFFF',
-          }}>
-          <Text>{`[${item.type}] ${item.description}`}</Text>
-        </G>
-      ))}
+    <G gap={0}>
+      <Scrollbars>
+        {stack.map((item, i) => (
+          <G
+            key={objectKey(item)}
+            style={{
+              width: '100%',
+              height: 32,
+              alignContent: 'center',
+              paddingLeft: 8,
+              color: next === i + 1 ? 'rgba(0, 100, 255)' : '#000000',
+              backgroundColor: next === i + 1 ? 'rgba(0, 100, 255, 0.1)' : '#FFFFFF',
+            }}>
+            <Text>{`[${item.type}] ${item.description}`}</Text>
+          </G>
+        ))}
+      </Scrollbars>
     </G>
   )
 })
