c6f0ba48f3b7a0c749936cca8892af5cd06e2fe9
Gitborlando <2629888564@qq.com>
2025-11-01 18:54:45 +0800

    去除无用依赖

--- src/view/shadcn/lib/utils.ts
+++ src/view/shadcn/lib/utils.ts
@@ -1,6 +1,5 @@
-import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: string[]) {
+  return twMerge(cx(...inputs))
 }
