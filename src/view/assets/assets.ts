224741d257caccadf6613071eff4962579829b57
Gitborlando <2629888564@qq.com>
2025-11-13 23:42:56 +0800

    changed UI assets

--- src/view/assets/assets.ts
+++ src/view/assets/assets.ts
@@ -1,78 +1,79 @@
 // 自动生成的静态资源路径常量
-import editorCursorResize from './editor/cursor/resize.svg'
-import editorCursorSelect from './editor/cursor/select.svg'
-import editorHeaderStageOperateMove from './editor/header/stage-operate/move.svg'
-import editorHeaderStageOperateSelect from './editor/header/stage-operate/select.svg'
-import editorNodeEllipse from './editor/node/ellipse.svg'
-import editorNodeFrame from './editor/node/frame.svg'
-import editorNodeImage from './editor/node/image.svg'
-import editorNodeLine from './editor/node/line.svg'
-import editorNodePolygon from './editor/node/polygon.svg'
-import editorNodeRect from './editor/node/rect.svg'
-import editorNodeStar from './editor/node/star.svg'
-import editorNodeText from './editor/node/text.svg'
-import editorRightPanelOperateAlignAlignCenter from './editor/right-panel/operate/align/align-center.svg'
-import editorRightPanelOperateAlignAlignLeft from './editor/right-panel/operate/align/align-left.svg'
-import editorRightPanelOperateAlignAlignRight from './editor/right-panel/operate/align/align-right.svg'
-import editorRightPanelOperateAlignVerticalBottom from './editor/right-panel/operate/align/vertical-bottom.svg'
-import editorRightPanelOperateAlignVerticalCenter from './editor/right-panel/operate/align/vertical-center.svg'
-import editorRightPanelOperateAlignVerticalTop from './editor/right-panel/operate/align/vertical-top.svg'
-import editorRightPanelOperateFillNone from './editor/right-panel/operate/fill/none.png'
-import editorRightPanelOperateGeoX from './editor/right-panel/operate/geo/x.svg'
-import editorRightPanelOperatePickerDefaultImage from './editor/right-panel/operate/picker/default-image.png'
-import favIconShiyangyang from './fav-icon/shiyangyang.png'
-import favIconSigma2 from './fav-icon/sigma-2.svg'
-import favIconSigma4 from './fav-icon/sigma-4.svg'
-import favIconSigma from './fav-icon/sigma.svg'
+import editorCursorResize from './editor/cursor/resize.svg';
+import editorCursorSelect from './editor/cursor/select.svg';
+import editorHeaderStageOperateMove from './editor/header/stage-operate/move.svg';
+import editorHeaderStageOperateSelect from './editor/header/stage-operate/select.svg';
+import editorNodeEllipse from './editor/node/ellipse.svg';
+import editorNodeFrame from './editor/node/frame.svg';
+import editorNodeImage from './editor/node/image.svg';
+import editorNodeLine from './editor/node/line.svg';
+import editorNodePolygon from './editor/node/polygon.svg';
+import editorNodeRect from './editor/node/rect.svg';
+import editorNodeStar from './editor/node/star.svg';
+import editorNodeText from './editor/node/text.svg';
+import editorRpOperateAlignAlignCenter from './editor/RP/operate/align/align-center.svg';
+import editorRpOperateAlignAlignLeft from './editor/RP/operate/align/align-left.svg';
+import editorRpOperateAlignAlignRight from './editor/RP/operate/align/align-right.svg';
+import editorRpOperateAlignVerticalBottom from './editor/RP/operate/align/vertical-bottom.svg';
+import editorRpOperateAlignVerticalCenter from './editor/RP/operate/align/vertical-center.svg';
+import editorRpOperateAlignVerticalTop from './editor/RP/operate/align/vertical-top.svg';
+import editorRpOperateFillNone from './editor/RP/operate/fill/none.png';
+import editorRpOperateGeoX from './editor/RP/operate/geo/x.svg';
+import editorRpOperatePickerDefaultImage from './editor/RP/operate/picker/default-image.png';
+import favIconShiyangyang from './fav-icon/shiyangyang.png';
+import favIconSigma2 from './fav-icon/sigma-2.svg';
+import favIconSigma4 from './fav-icon/sigma-4.svg';
+import favIconSigma from './fav-icon/sigma.svg';
 
 export const Assets = {
-  editor: {
-    cursor: {
-      resize: editorCursorResize,
-      select: editorCursorSelect,
+  "editor": {
+    "cursor": {
+      "resize": editorCursorResize,
+      "select": editorCursorSelect,
     },
-    header: {
-      stageOperate: {
-        move: editorHeaderStageOperateMove,
-        select: editorHeaderStageOperateSelect,
+    "header": {
+      "stageOperate": {
+        "move": editorHeaderStageOperateMove,
+        "select": editorHeaderStageOperateSelect,
       },
     },
-    node: {
-      ellipse: editorNodeEllipse,
-      frame: editorNodeFrame,
-      image: editorNodeImage,
-      line: editorNodeLine,
-      polygon: editorNodePolygon,
-      rect: editorNodeRect,
-      star: editorNodeStar,
-      text: editorNodeText,
+    "node": {
+      "ellipse": editorNodeEllipse,
+      "frame": editorNodeFrame,
+      "image": editorNodeImage,
+      "line": editorNodeLine,
+      "polygon": editorNodePolygon,
+      "rect": editorNodeRect,
+      "star": editorNodeStar,
+      "text": editorNodeText,
     },
-    rightPanel: {
-      operate: {
-        align: {
-          alignCenter: editorRightPanelOperateAlignAlignCenter,
-          alignLeft: editorRightPanelOperateAlignAlignLeft,
-          alignRight: editorRightPanelOperateAlignAlignRight,
-          verticalBottom: editorRightPanelOperateAlignVerticalBottom,
-          verticalCenter: editorRightPanelOperateAlignVerticalCenter,
-          verticalTop: editorRightPanelOperateAlignVerticalTop,
+    "RP": {
+      "operate": {
+        "align": {
+          "alignCenter": editorRpOperateAlignAlignCenter,
+          "alignLeft": editorRpOperateAlignAlignLeft,
+          "alignRight": editorRpOperateAlignAlignRight,
+          "verticalBottom": editorRpOperateAlignVerticalBottom,
+          "verticalCenter": editorRpOperateAlignVerticalCenter,
+          "verticalTop": editorRpOperateAlignVerticalTop,
         },
-        fill: {
-          none: editorRightPanelOperateFillNone,
+        "fill": {
+          "none": editorRpOperateFillNone,
         },
-        geo: {
-          x: editorRightPanelOperateGeoX,
+        "geo": {
+          "x": editorRpOperateGeoX,
         },
-        picker: {
-          defaultImage: editorRightPanelOperatePickerDefaultImage,
+        "picker": {
+          "defaultImage": editorRpOperatePickerDefaultImage,
         },
       },
     },
   },
-  favIcon: {
-    shiyangyang: favIconShiyangyang,
-    sigma2: favIconSigma2,
-    sigma4: favIconSigma4,
-    sigma: favIconSigma,
+  "favIcon": {
+    "shiyangyang": favIconShiyangyang,
+    "sigma2": favIconSigma2,
+    "sigma4": favIconSigma4,
+    "sigma": favIconSigma,
   },
-} as const
+
+} as const;
